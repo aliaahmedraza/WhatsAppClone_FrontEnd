3ae12db9ec2313e3ed2bf2855b4334977d0104fa
aliaahmedraza <aliaahmedraza@gmail.com>
2025-06-16 16:14:25 +0500

    I made a little chaning of css in the chat component

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -4,15 +4,16 @@ import MenuIcon from "../../assets/svg/MenuIcon";
 import Search from "../Search/Search";
 import ArchievedIcon from "../../assets/svg/ArchievedIcon";
 import ChatByPerson from "../ChatByPerson/ChatByPerson";
-import "./Chat.css"
+import "./Chat.css";
 
 const Chat = () => {
   const [selectedIcon, setSelectedIcon] = React.useState("");
-  const [selectedButton, setSelectedButton]= React.useState("All")
+  const [selectedButton, setSelectedButton] = React.useState("All");
+  const [searchPlaceHolder, setSearchPlaceHolder] = React.useState("Search");
   const buttons = ["All", "Unreads", "Favorites", "Groups"];
   return (
     <div className="container h-full w-full flex flex-col">
-      <div className="h-1/6 min-h-[100px]">
+      <div className="h-1/6 min-h-[100px] ">
         <div className="flex justify-between items-center h-1/3 mx-3">
           <div className="text-[#E9EDEF]">
             <h1 className="font-bold text-2xl sm:text-2xl">Chats</h1>
@@ -43,13 +44,18 @@ const Chat = () => {
           </div>
         </div>
         <div>
-          <Search placeHolder="Search" />
+          <Search placeHolder={searchPlaceHolder} />
         </div>
         <div className="flex gap-2.5 h-1/3 mx-3 items-center md:gap-2 md:pr-5">
           {buttons.map((btn, i) => (
             <div key={i}>
               <button
-                onClick={() => setSelectedButton(btn)}
+                onClick={() => {
+                  if (btn === "All") {
+                    setSearchPlaceHolder("Search");
+                  } else setSearchPlaceHolder(`Search ${btn} chats`);
+                  setSelectedButton(btn);
+                }}
                 className={`rounded-2xl h-9 p-2 text-sm flex items-center justify-center ${
                   selectedButton === btn
                     ? "bg-[#005C4B] text-[#8696A0]"
@@ -65,7 +71,7 @@ const Chat = () => {
       <div className="flex-1 overflow-auto custom-scrollbar mr-[2px] mt-4">
         <div className="flex ml-5 mr-[2px] gap-4">
           <ArchievedIcon />
-          <h1 className="text-[#E9EDEF] border-b-[#374248] border-b-1 w-full pb-5">
+          <h1 className="text-[#E9EDEF] border-b-[#374248] border-b-1 w-full pb-5 ml-1">
             Archieved
           </h1>
         </div>
--- src/components/ChatByPerson/ChatByPerson.jsx
+++ src/components/ChatByPerson/ChatByPerson.jsx
@@ -27,10 +27,10 @@ const ChatByPerson = () => {
     <div className="h-full w-full mt-2">
       {personChatDetail.map((details, i) => (
         <div key={i} className="flex mx-3 items-center h-18">
-          <div className="pb-2">
+          <div className="pb-3">
             <img src={details.src} className="h-10 w-11 rounded-[50%]" />
           </div>
-          <div className="flex flex-col gap-1 border-b-[#374248] border-b-1 w-full justify-center pb-2 ml-3">
+          <div className="flex flex-col gap-1 border-b-[#374248] border-b-1 w-full justify-center pb-2 ml-4">
             <div className="flex justify-between items-center">
               <h1 className="text-[#E9EDEF]">{details.name}</h1>
               <p className="text-[#8696A0] pr-3 text-sm">{details.time}</p>
