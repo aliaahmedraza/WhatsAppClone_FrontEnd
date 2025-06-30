d711c473036fa5dbf530ec6394934b38d10ebe9c
aliaahmedraza <aliaahmedraza@gmail.com>
2025-06-30 15:46:25 +0500

    Loader component

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,10 +3,6 @@ import React from "react";
 import "./Loading.css";
 const LoadingScreen = () => {
   return (
-    // <div className="h-screen w-screen flex flex-col items-center justify-center bg-[#0d1117] text-white">
-    //   <div className="text-6xl animate-bounce mb-4">🐱</div>
-    //   <p className="text-sm opacity-70">One moment please...</p>
-    // </div>
     <div className="h-screen w-screen bg-black flex items-center justify-center">
       <div className="spninner">
         <div></div>
